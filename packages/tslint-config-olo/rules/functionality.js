5dfb1a98b5afdb5c31f39e5e845b0aa78b6918d9
Greg Shackles <gshackles@gmail.com>
2016-07-31 16:33:04 -0400

    Add tslint-config-olo package

---
+++ packages/tslint-config-olo/rules/functionality.js
@@ -0,0 +1,33 @@
+module.exports = {
+  'rules': {
+    'ban': false,
+    'curly': true,
+    'forin': true,
+    'label-position': true,
+    'label-undefined': true,
+    'no-arg': true,
+    'no-bitwise': true,
+    'no-conditional-assignment': true,
+    'no-console': true,
+    'no-construct': true,
+    'no-debugger': true,
+    'no-duplicate-key': true,
+    'no-duplicate-variable': true,
+    'no-eval': true,
+    'no-invalid-this': true,
+    'no-null-keyword': true,
+    'no-shadowed-variable': true,
+    'no-string-literal': false,
+    'no-switch-case-fall-through': true,
+    'no-unreachable': true,
+    'no-unused-expression': true,
+    'no-unused-variable': [true, 'react', { 'ignore-pattern': 'React' }],
+    'no-use-before-declare': true,
+    'no-var-keyword': true,
+    'radix': true,
+    'switch-default': true,
+    'triple-equals': true,
+    'use-isnan': true,
+    'use-strict': true
+  }
+};
---
+++ packages/tslint-config-olo/rules/maintainability.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'rules': {
+    'eofline': false,
+    'indent': [true, 'spaces'],
+    'linebreak-style': false,
+    'max-line-length': false,
+    'no-default-export': false,
+    'no-mergeable-namespace': false,
+    'no-require-imports': true,
+    'no-trailing-whitespace': true,
+    'object-literal-sort-keys': false,
+    'trailing-comma': false
+  }
+};
